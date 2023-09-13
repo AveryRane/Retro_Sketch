1dda4e392753c9056321fbd092b5d6923b36f921
AveryRane <averyrane18@gmail.com>
2023-09-12 21:46:03 -0500

    improve divs

---
+++ index.js
@@ -0,0 +1,39 @@
+const sketchPad = document.querySelector('#sketchPad');
+
+const resSelector = document.querySelector('.slider');
+const submit = document.querySelector('#submit');
+
+resSelector.addEventListener('click', getRes);
+
+let rows = [];
+
+function getRes() {
+	submit.value = resSelector.value;
+	rows = [];
+	let oldValue = document.querySelectorAll('.rows');
+	console.log(oldValue.length);
+	oldValue.forEach(div => {
+		div.remove();
+	});
+	if (rows.length === 0) {
+		for (let x = 0; x < resSelector.value; x++) {
+			rows.push(x + 1);
+		}
+		console.log('rows: ' + rows.length);
+	}
+}
+
+submit.addEventListener('click', () => {
+	rows.forEach(row => {
+		const newRow = document.createElement('div');
+		newRow.classList.add('rows');
+		sketchPad.appendChild(newRow);
+	});
+sketchPad.childNodes.forEach(child => {
+		const newCol = document.createElement('div');
+		newCol.classList.add('columns');
+		child.appendChild(newCol);
+	});
+});
+for (var i = 0; i < rows.length; i++) 
+sketchPad.childNodes[i]); // Note that this does NOT go to the DOM
