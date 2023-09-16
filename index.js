b3e82f92ecfcd11dc1f3c2bee653582b90b8b63f
AveryRane <averyrane18@gmail.com>
2023-09-15 20:23:06 -0500

    edit done as of 9-15-23

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
+// get grid container
 const sketchPad = document.querySelector('#sketchPad');
 
-const resSelector = document.querySelector('.slider');
-const submit = document.querySelector('#submit');
-
-resSelector.addEventListener('click', getRes);
-
+// variables for grid and color
 let rows = [];
+let currentColor = '#000000';
 
-function getRes() {
-	submit.value = resSelector.value;
-	rows = [];
-	let oldValue = document.querySelectorAll('.rows');
-	console.log(oldValue.length);
-	oldValue.forEach(div => {
-		div.remove();
-	});
-	if (rows.length === 0) {
-		for (let x = 0; x < resSelector.value; x++) {
-			rows.push(x + 1);
-		}
-		console.log('rows: ' + rows.length);
-	}
+// handles grid resizing
+const resSelector = document.querySelector('.slider');
+resSelector.addEventListener('click', resize);
+
+// needs refining sets erasor
+const erasor = document.querySelector('#toggle');
+
+erasor.addEventListener('click', event => {
+	currentColor = '#f5f5f5';
+});
+
+// set color
+const colorSelected = document.querySelector('#colors');
+colorSelected.addEventListener('change', watchColorPicker, false);
+function watchColorPicker(event) {
+	erasor.checked = false;
+	currentColor = event.target.value;
 }
 
-submit.addEventListener('click', () => {
+window.onload = event => {
+	setRes();
+};
+
+// resize grid
+function resize() {
+	currentColor = colorSelected.value;
+	erasor.checked = false;
+	rows = [];
+	let gridExists = document.querySelectorAll('.rows');
+	console.log('step 1a');
+	gridExists.forEach(div => {
+		div.remove();
+	});
+	setRes();
+}
+
+function setRes() {
+	for (let x = 0; x < resSelector.value; x++) {
+		rows.push(x + 1);
+	}
+	setGrid();
+}
+
+// sets grid layout and adds mouseover event listeners to columns
+function setGrid() {
 	rows.forEach(row => {
 		const newRow = document.createElement('div');
 		newRow.classList.add('rows');
@@ -33,10 +59,10 @@ submit.addEventListener('click', () => {
 		for (let i = 0; i < rows.length; i++) {
 			const newCol = document.createElement('div');
 			newCol.addEventListener('mouseenter', e => {
-				newCol.style.backgroundColor = 'black';
+				newCol.style.backgroundColor = currentColor;
 			});
 			newCol.classList.add('columns');
 			child.appendChild(newCol);
 		}
 	});
-});
+}
